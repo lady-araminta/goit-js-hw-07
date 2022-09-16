d0e753fc7ed48bab406d1517be884863f666bed6
lady-araminta <jparkhomenko@gmail.com>
2022-09-16 14:34:27 +0300

    Update gallery

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,42 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
+
+const galleryContRef = document.querySelector('.gallery');
+
+const galleryMarkup = galleryItems
+  .map(
+    ({ preview, original, description }) =>
+      `
+<div class="gallery__item">
+  <a class="gallery__link" href="${original}">
+    <img
+      class="gallery__image"
+      src="${preview}"
+      data-source="${original}"
+      alt="${description}"
+    />
+  </a>
+</div>
+`
+  )
+  .join('');
+
+galleryContRef.insertAdjacentHTML('afterbegin', galleryMarkup);
+
+galleryContRef.addEventListener('click', onImgClick);
+
+function onImgClick(event) {
+  event.preventDefault();
+  const { target } = event;
+  const largeImg = target.dataset.source;
+  const alt = target.alt;
+  if (target.nodeName !== 'IMG') {
+    return;
+  }
+  const instance = basicLightbox.create(`
+    <img src="${largeImg}" alt="${alt}">
+  `);
+
+  instance.show();
+}
 
 console.log(galleryItems);
